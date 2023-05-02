afff594d178574056307acf1f4e25698276dc26c
iand66 <idavies66@gmail.com>
2023-05-02 20:22:49 +0100

    Initial Commit

---
+++ test_server.py
@@ -0,0 +1,9 @@
+import requests
+
+# print(requests.get("http://127.0.0.1:8000/").json())
+# print(requests.get("http://127.0.0.1:8000/items/0").json())
+# print(requests.get("http://127.0.0.1:8000/items/1").json())
+# print(requests.get("http://127.0.0.1:8000/items/2").json())
+# print(requests.get("http://127.0.0.1:8000/items/fred").json())
+# print(requests.get("http://127.0.0.1:8000/items?name=Nails").json())
+
